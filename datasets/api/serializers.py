d8a71c51399ad4183e062c7aa77fb3d0c9d521e7
ostlight <rhainich@astro.physik.uni-potsdam.de>
2025-06-05 20:23:09 +0200

    Adds test fix for NaN serialization issue

--- datasets/api/serializers.py
+++ datasets/api/serializers.py
@@ -1,9 +1,9 @@
-from rest_framework.serializers import ModelSerializer
-
+from rest_framework import serializers
 from datasets.models import Dataset
+import numpy as np
 
 
-class DatasetSerializer(ModelSerializer):
+class DatasetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dataset
         fields = [
@@ -15,6 +15,17 @@ class DatasetSerializer(ModelSerializer):
             'illuminance',
             'wind_speed',
             'rain',
+            'note',
             'merged',
+            'added_on',
+            'last_modified',
             ]
         read_only_fields = ('pk',)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        # Convert NaN values to None (which becomes null in JSON)
+        for key, value in data.items():
+            if isinstance(value, float) and (np.isnan(value) or np.isinf(value)):
+                data[key] = None
+        return data
